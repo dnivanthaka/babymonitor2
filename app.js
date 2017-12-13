3681e5cbc589e74953b55891ee87c67d4f0f45b0
Dinusha Nivanthaka Amerasinghe <nivanthaka@gmail.com>
2017-12-13 22:39:55 +0530

    Updated sources

--- app.js
+++ app.js
@@ -6,12 +6,24 @@ var app = express();
 var sockIo = require('socket.io');
 
 var am2320 = require('./am2320.js');
+var pir = require('./pir.js');
+
+var motion = {
+        this.state = 0;
+};
+
+motion.update = function(newstate){
+        this.state = newstate;
+        console.log('Motion event ' + this.state);
+}
 
 
 app.use(express.static('html'));
 app.get('/', function(req, res){
     res.end();
 });
+
+pir.setup(motion.update);
 
 // AM2320 Data as json object
 var am2320Data = null;
---
+++ pir.js
@@ -0,0 +1,58 @@
+//const fs = require('fs');
+var rpio = require('rpio');
+
+rpio.init({mapping: 'physical'});
+
+const PIRPIN = 3;
+/*const gpioSysFs = "/sys/class/gpio/";
+
+//Settingup GPIO
+function setupGPIO(pin, dir, cb_onerror, cb_onsuccess){    
+        var self = this;        
+
+        //Check if already exported
+        if(!fs.existsSync(gpioSysFs + "gpio" + pin + "/direction")){
+                fs.writeFile(gpioSysFs + "export", pin, (err) => {
+                        if(err) {
+                                cb_onerror(self, err);
+                                return;
+                        }
+                         fs.writeFileSync(gpioSysFs + "gpio" + pin + "/direction", dir);
+                        cb_onsuccess(self);
+                });
+               
+        }else{
+                //Already exported
+                fs.readFile(gpioSysFs + "gpio" + pin + "/direction", (err, data) => {
+                        if(err) {
+                                cb_onerror(self, err);
+                                return;
+                        }
+                        
+                        if(data !== dir){
+                                console.log('Already exported as different pin direction');
+                                return;
+                        }
+                        
+                });
+        }
+}*/
+
+/*function pollcb(){
+        var state = rpio.read(PIRPIN);
+        
+        console.log(state);
+}*/
+module.exports = {
+        setup: function(cb_poll){
+                rpio.open(PIRPIN, rpio.INPUT, rpio.PULL_UP);
+                rpio.poll(PIRPIN,  function(){
+                        var state = rpio.read(PIRPIN);
+                        cb_poll(state);
+                });
+        },
+        cleanup: function(){
+                rpio.close(PIRPIN);
+        }
+}
+//setupGPIO('11', 'out', function(self, err){console.log('Error: '+err);}, function(self){console.log('Success: ');});
