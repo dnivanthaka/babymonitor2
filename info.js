e8f12dcc9f29b0c4f96ee6df1d4ee038de2c7a28
Dinusha Nivanthaka Amerasinghe <dinusha@dinusha-HP-Pro-3130-Microtower-PC>
2017-12-13 17:55:19 +0530

    Updated sources

--- info.js
+++ info.js
@@ -1,10 +1,10 @@
 const { spawn } = require('child_process');
 //iw wlan0 link | grep SSID
 //ifconfig wlan0 | grep -o 'inet addr:[0-9]*.[0-9]*.[0-9]*.[0-9]*'
-function exec_simple_cmd(pcmd, error, stdout, stderr){
+function exec_simple_cmd(pcmd, args){
         self = this;
 
-        const cmd = spawn(pcmd, []);
+        const cmd = spawn(pcmd, args);
 
         cmd.on('error', (data)=>{
                 console.log(`error: ${data}`);
@@ -12,7 +12,7 @@ function exec_simple_cmd(pcmd, error, stdout, stderr){
 
         cmd.stdout.on('data', (data) => {
           console.log(`stdout: ${data}`);
-           stdout(data);
+           //stdout(data);
         });
 
         cmd.stderr.on('data', (data) => {
@@ -24,6 +24,17 @@ function exec_simple_cmd(pcmd, error, stdout, stderr){
         });
 }
 
+const { exec } = require('child_process');
+exec('ifconfig eth0 | grep -o "inet addr:[0-9]*.[0-9]*.[0-9]*.[0-9]*"', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+});
+
+
 var obj = {};
 
 obj.exec = function(cb){
@@ -31,7 +42,7 @@ obj.exec = function(cb){
         
 }
 
-exec_simple_cmd("lsx", function(e){console.log("Error "+e)},function(e){console.log("Stdout "+e)} , function(e){console.log("Stderr "+e)});
+//exec_simple_cmd("ifconfig", ['eth0', '|', 'grep', '-o', "'inet addr:[0-9]*.[0-9]*.[0-9]*.[0-9]*'"]);
 
 //var test = new obj();
 obj.exec(function(){
