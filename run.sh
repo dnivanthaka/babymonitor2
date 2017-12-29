68ff510273c55798c8720804ebb3e1bdf30d55b7
dnivanthaka <nivanthaka@gmail.com>
2017-12-29 22:37:59 +0530

    Updated sources

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export PATH=$PATH:/usr/local/bin/node-v6.11.0-linux-armv6l/bin
+export PATH=$PATH:/usr/bin:/usr/local/bin/node-v6.11.0-linux-armv6l/bin
 cd /home/pi/projects/babymonitor2
 #Check for updates first
-./update.sh
+git pull origin master
 node ./app.js &
 #sleep 5
 #iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
